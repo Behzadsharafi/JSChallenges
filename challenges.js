3947204ce67005c9a5816c9a0629a59937eebcfe
Behzad Sharafi <behzadsharafi@gmail.com>
2023-11-22 14:58:38 +1100

    adds solutions

--- challenges.js
+++ challenges.js
@@ -235,3 +235,29 @@ const findOdd2 = (A) => {
     if (obj[prop] % 2 !== 0) return Number(prop);
   }
 };
+
+// You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+// Examples
+// [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+
+// [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+
+const findOutlier = (array) => {
+  let sum = 0;
+
+  for (let i = 0; i < 3; i++) {
+    sum += array[i] % 2;
+  }
+
+  const mod = sum === 1 || sum === 0 ? 1 : 0;
+
+  //We could use filter but this method is less costly
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] % 2 === mod) {
+      return array[i];
+    }
+  }
+
+  return null;
+};
--- index.js
+++ index.js
@@ -1,12 +1,33 @@
-const findOdd = (A) => {
-  let obj = {};
-  A.forEach((el) => {
-    obj[el] ? obj[el]++ : (obj[el] = 1);
-  });
-
-  for (prop in obj) {
-    if (obj[prop] % 2 !== 0) return Number(prop);
+const findOutlier = (array) => {
+  let evenCount = 0;
+  let oddCount = 0;
+  for (let i = 0; i < 3; i++) {
+    if (array[i] % 2 == 0) {
+      evenCount++;
+    } else {
+      oddCount++;
+    }
   }
+
+  let isOutlierEven = evenCount > oddCount;
+  for (let num of array) {
+    if (isOutlierEven && num % 2 != 0) {
+      return num;
+    } else if (!isOutlierEven && num % 2 == 0) {
+      return num;
+    }
+  }
+
+  throw new IllegalArgumentException("No outlier found.");
+
+  // const sum = array
+  //   .slice(0, 3)
+  //   .map((num) => num % 2)
+  //   .reduce((acc, curr) => acc + curr);
+
+  // const mod = sum === 1 || sum === 0 ? 1 : 0;
+
+  // return array.filter((num) => num % 2 === mod)[0];
 };
 
-console.log(findOdd([1, 2, 2, 3, 3, 3, 4, 3, 3, 3, 2, 2, 1]));
+console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]));
